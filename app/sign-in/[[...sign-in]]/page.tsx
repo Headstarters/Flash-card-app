0c87b8309b9b2ff0e749124996d3a269d87bfefc
Markus <berriosmarkus@gmail.com>
2024-08-18 17:33:09 -0400

    Added light/dark mode

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -7,7 +7,6 @@ export default function SignInPage() {
   return (
     <>
     <Container
-   
     >
         <AppBar position="static">
             <Toolbar>   
--- app/theme.js
+++ app/theme.js
@@ -1,16 +1,35 @@
-// theme.js
 import { createTheme } from '@mui/material/styles';
 
-// Create a theme instance with Poppins font
-const theme = createTheme({
+// Light Theme
+const lightTheme = createTheme({
   typography: {
     fontFamily: "'Poppins', sans-serif",
   },
   palette: {
+    mode: 'light',
     background: {
-      default: '#bbe7fc', // Apply your background color here
+      default: '#e3f2fd',
+    },
+    primary: {
+      main: '#009688',
     },
   },
 });
 
-export default theme;
+// Dark Theme
+const darkTheme = createTheme({
+  typography: {
+    fontFamily: "'Poppins', sans-serif",
+  },
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#1b1b1b',
+    },
+    primary: {
+      main: '#ffc107',
+    },
+  },
+});
+
+export { lightTheme, darkTheme };
